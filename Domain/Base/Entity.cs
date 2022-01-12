5897f36f161106946c8e458a526bffa43ef664c1
Hakan Eren <hakan.eren@oplog.com.tr>
2022-01-12 20:37:22 +0300

    new properties added to customer entity

--- Domain/Base/Entity.cs
+++ Domain/Base/Entity.cs
@@ -10,9 +10,13 @@ namespace Domain.Base
     public abstract class Entity 
     {
         public DateTime CreatedDate { get; set; }
+
         public string CreatedBy { get; set; }
+
         public DateTime? UpdatedDate { get; set; }
+
         public string UpdatedBy { get; set; }
+
         public bool IsDeleted { get; set; }
     }
 }
--- Domain/Entities/Customer/Customer.cs
+++ Domain/Entities/Customer/Customer.cs
@@ -1,9 +1,41 @@
 ﻿using Domain.Base;
+using System;
 
 namespace Domain.Entities.Customer
 {
     public abstract class Customer : Entity<int>
     {
+        public string FirstName { get; set; }
 
+        public string LastName { get; set; }
+
+        public string FullName { get; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Email { get; set; }
+
+        public string MobileCountryCode { get; set; }
+
+        public string MobileAreaCode { get; set; }
+
+        public string MobileNumber { get; set; }
+
+        public string NationalityCode { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                DateTime nowDate = DateTime.Now;
+
+                int age = nowDate.Year - BirthDate.Year;
+
+                if (nowDate.Month < BirthDate.Month || (nowDate.Month == BirthDate.Month && nowDate.Day < BirthDate.Day))
+                    age--;
+
+                return age;
+            }
+        }
     }
 }
