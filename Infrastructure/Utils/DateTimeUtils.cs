d2da098658cdb7f8c5057f342f6c25d299a90ce6
Hakan Eren <hakan.eren@oplog.com.tr>
2022-01-17 00:07:22 +0300

    DateTime utils implemented

---
+++ Infrastructure/Utils/DateTimeUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Infrastructure.Utils
+{
+    public static class DateTimeUtils
+    {
+        public static bool IsNotDefaultDate(DateTime value)
+        {
+            return !value.Equals(default(DateTime));
+        }
+    }
+}
