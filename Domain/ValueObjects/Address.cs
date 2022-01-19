7a64dac39f3011289a3dae8fddd170337aabfe13
Hakan Eren <hakan.eren@oplog.com.tr>
2022-01-19 20:14:28 +0300

    Value object base class implemented

--- Domain/ValueObjects/Address.cs
+++ Domain/ValueObjects/Address.cs
@@ -1,6 +1,9 @@
-﻿namespace Domain.ValueObjects
+﻿using Domain.Base;
+using System.Collections.Generic;
+
+namespace Domain.ValueObjects
 {
-    public class Address
+    public class Address : ValueObject
     {
         public string City { get; set; }
         public string Town { get; set; }
@@ -8,5 +11,15 @@
         public string Street { get; set; }
         public string GateNumber { get; set; }
         public string ApartmentNumber { get; set; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return City;
+            yield return Town;
+            yield return Neighborhood;
+            yield return Street;
+            yield return GateNumber;
+            yield return ApartmentNumber;
+        }
     }
 }
